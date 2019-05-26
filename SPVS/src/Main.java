7192ad1fc1baa02efebbaa202e391d80be283fbc
seba-murillo <sebamur@hotmail.com>
2019-05-26 07:11:38 -0300

    added docs

---
+++ SPVS/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main{
+
+	public static void main(String[] args){
+		log("hello world");
+	}
+	
+	public static void log(Object message) {
+		System.out.println(message.toString());
+	}
+}
