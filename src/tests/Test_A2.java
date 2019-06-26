7f88de52a609dac756c548e25c741c985923c7b4
seba-murillo <sebamur@hotmail.com>
2019-06-26 11:55:20 -0300

    improved graphics

---
+++ src/tests/Test_A2.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import model.*;
+import model.Game;
+import model.State;
+
+
+class Test_A2{
+
+	@Test
+	void test(){
+		new Game(20, 20);
+		State state = Game.getCurrentState();
+		assert(state.addEntity(new Rabbit(), 5, 5));
+		assert(state.addEntity(new Rabbit(), 0, 3));
+		assert(state.addEntity(new Rabbit(), 3, 8));
+		assert(state.addEntity(new Wolf(), 7, 8));
+	}
+}
